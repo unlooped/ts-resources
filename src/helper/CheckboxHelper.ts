9512f54166a099c95d776fd68738940505cb03c2
Steffen Persch <s.persch@unlooped.com>
2022-06-11 14:05:17 +0200

    [ENH] support indeterminate checkboxes

--- src/helper/CheckboxHelper.ts
+++ src/helper/CheckboxHelper.ts
@@ -1,7 +1,11 @@
 import * as jQuery from "jquery";
 import ClickEvent = JQuery.ClickEvent;
+import ChangeEvent = JQuery.ChangeEvent;
 
 class CheckboxHelper {
+
+    private static toggleCheckboxSelector = 'input[type="checkbox"][data-action^="checkbox:toggle"]';
+
     constructor() {
         this.loadElements();
         this.loadEvents();
@@ -13,13 +17,17 @@ class CheckboxHelper {
     }
 
     private loadEvents() {
-        jQuery(document.body).on('click', '*[data-action^="checkbox:select-all"]', this.selectAll.bind(this));
-        jQuery(document.body).on('click', '*[data-action^="checkbox:select-none"]', this.selectNone.bind(this));
-        jQuery(document.body).on('click', '*[data-action^="checkbox:toggle"]', this.toggle.bind(this));
+        jQuery(document.body).on('click', 'button[data-action^="checkbox:select-all"]', this.selectAll.bind(this));
+        jQuery(document.body).on('click', 'button[data-action^="checkbox:select-none"]', this.selectNone.bind(this));
+        jQuery(document.body).on('click', 'button[data-action^="checkbox:toggle"]', this.toggle.bind(this));
+        jQuery(document.body).on('change', CheckboxHelper.toggleCheckboxSelector, this.toggleCheckboxChanged.bind(this));
+        jQuery(document.body).on('change', 'input[type="checkbox"]', this.generalCheckboxChanged.bind(this));
     }
 
     private init() {
-
+        document.querySelectorAll(CheckboxHelper.toggleCheckboxSelector).forEach((el: HTMLInputElement) => {
+            this.updateToggleCheckboxStatus(el);
+        });
     }
 
     private selectAll(e: ClickEvent) {
@@ -28,6 +36,10 @@ class CheckboxHelper {
         checkboxes.forEach((cb) => {
             cb.checked = true;
         });
+
+        if (checkboxes.length > 0) {
+            this.updateRelatedToggleCheckboxes(checkboxes[0]);
+        }
     }
 
     private selectNone(e: ClickEvent) {
@@ -36,6 +48,10 @@ class CheckboxHelper {
         checkboxes.forEach((cb) => {
             cb.checked = false;
         });
+
+        if (checkboxes.length > 0) {
+            this.updateRelatedToggleCheckboxes(checkboxes[0]);
+        }
     }
 
     private toggle(e: ClickEvent) {
@@ -44,11 +60,62 @@ class CheckboxHelper {
         checkboxes.forEach((cb) => {
             cb.checked = !cb.checked;
         });
+
+        if (checkboxes.length > 0) {
+            this.updateRelatedToggleCheckboxes(checkboxes[0]);
+        }
     }
 
-    private getCheckboxesForEvent(e: ClickEvent): NodeListOf<HTMLInputElement> {
-        let btn = e.currentTarget;
-        return  document.querySelectorAll('input[type="checkbox"]' + btn.dataset.checkboxToggleTarget);
+    private toggleCheckboxChanged(e: ChangeEvent) {
+        let currentTarget = e.currentTarget;
+        let checkboxes = this.getCheckboxesForEvent(e);
+        checkboxes.forEach((cb) => {
+            cb.checked = currentTarget.checked;
+        });
+    }
+
+    private getCheckboxesForEvent(e: ClickEvent|ChangeEvent): NodeListOf<HTMLInputElement> {
+        let source = e.currentTarget;
+        return  document.querySelectorAll('input[type="checkbox"]' + source.dataset.checkboxToggleTarget);
+    }
+
+    private generalCheckboxChanged(e: ChangeEvent) {
+        let currentTarget = e.currentTarget;
+        this.updateRelatedToggleCheckboxes(currentTarget);
+    }
+
+    private updateRelatedToggleCheckboxes(cb: HTMLInputElement) {
+        let matches = [];
+        document.querySelectorAll(CheckboxHelper.toggleCheckboxSelector).forEach((el: HTMLInputElement) => {
+            if (cb.matches(el.dataset.checkboxToggleTarget)) {
+                matches.push(el);
+            }
+        });
+
+        if (matches.length === 0) {
+            return;
+        }
+
+        matches.forEach((match) => {
+            this.updateToggleCheckboxStatus(match);
+        })
+
+    }
+
+    private updateToggleCheckboxStatus(toggleCheckbox: HTMLInputElement) {
+        let totalTargets = document.querySelectorAll('input[type="checkbox"]' + toggleCheckbox.dataset.checkboxToggleTarget);
+        let checkedTargets = document.querySelectorAll('input[type="checkbox"]' + toggleCheckbox.dataset.checkboxToggleTarget + ':checked');
+
+        if (checkedTargets.length === 0) {
+            toggleCheckbox.indeterminate = false;
+            toggleCheckbox.checked = false;
+        } else if (checkedTargets.length < totalTargets.length) {
+            toggleCheckbox.indeterminate = true;
+            toggleCheckbox.checked = false;
+        } else if (checkedTargets.length === totalTargets.length) {
+            toggleCheckbox.indeterminate = false;
+            toggleCheckbox.checked = true;
+        }
     }
 
 }
